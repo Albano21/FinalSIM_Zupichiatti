19467eb9cf94f6e67bacfe7a0e63e7f54b787190
Albano Zupichiatti <89983599+Albano21@users.noreply.github.com>
2022-06-28 19:02:40 -0300

    Initial commit

---
+++ src/main/java/finalsim/finalsim_zupichiatti/FinalSimZupichiattiApplication.java
@@ -0,0 +1,13 @@
+package finalsim.finalsim_zupichiatti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSimZupichiattiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSimZupichiattiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finalsim/finalsim_zupichiatti/FinalSimZupichiattiApplicationTests.java
@@ -0,0 +1,13 @@
+package finalsim.finalsim_zupichiatti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSimZupichiattiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

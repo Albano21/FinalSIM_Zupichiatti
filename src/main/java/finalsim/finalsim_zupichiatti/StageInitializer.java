a77fbd8eef621a172a0432178ffe10f3a43d820c
Albano Zupichiatti <89983599+Albano21@users.noreply.github.com>
2022-07-01 20:50:40 -0300

    correccion en StageInitializer por error en StageManager

--- src/main/java/finalsim/finalsim_zupichiatti/StageInitializer.java
+++ src/main/java/finalsim/finalsim_zupichiatti/StageInitializer.java
@@ -1,17 +1,42 @@
 package finalsim.finalsim_zupichiatti;
 
 import finalsim.finalsim_zupichiatti.FinalSimFxApplication.StageReadyEvent;
+import finalsim.finalsim_zupichiatti.utils.StageManager;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
 
+    @Value("classpath:/fxml/main.fxml")
+    private Resource sceneResource;
+
+    private final ApplicationContext applicationContext;
+
+    public StageInitializer(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     @Override
     public void onApplicationEvent(StageReadyEvent event) {
-        Stage stage = event.getStage();
+
+        try {
+            Stage stage = event.getStage();
+            StageManager stageManager = applicationContext.getBean(StageManager.class, stage, applicationContext);
+
+            stageManager.loadStageParentScene(sceneResource.getURL(), 500, 600);
+            stageManager.showStage();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
